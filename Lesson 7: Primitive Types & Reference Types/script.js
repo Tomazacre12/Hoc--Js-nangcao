38c131e54c11f7c21fbfe5c5fa04d0e76d5b9d01
Tomazacre12 <shadow12122004@gmail.com>
2022-09-02 23:07:01 +0700

    add

---
+++ Lesson 7: Primitive Types & Reference Types/script.js
@@ -0,0 +1,44 @@
+// value types
+let a = 1
+let b = a
+a = 2
+console.log(b)
+
+// reference types
+
+let c = {
+    name: 'Heracules' //#001
+}
+let d = c //#001
+c.name = 'Hades' //#001
+console.log(d)
+
+let e = {
+    name: 'Vũ Trí Ba Tá Trợ', //#001
+}
+e = {
+    name: 'Tuyền Qua Minh Nhân', //#002
+    age: '17'
+}
+
+const student = {
+    name: 'Flandre Scarlet',
+    profile: {
+        first: 'Flandre',
+        last: 'Scarlet',
+        age: '405'
+    }
+}
+const studentProfile = student.profile
+student.profile.age = '400'
+console.log(student.profile.age)
+
+function sum(f, g){
+     f = 0,
+     g = 0,
+    console.log(f, g)
+}
+const h = 1
+const i = 2
+sum(h, i)
+console.log(h, i)
